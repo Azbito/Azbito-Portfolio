9228acb78d89d012c96e9e1acc1a4e0aa1ff562f
Thiago <azbitomsc@gmail.com>
2023-03-10 22:42:30 -0300

    Update

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,10 +1,34 @@
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import styles from './navbar.module.scss'
+import { animateScroll as scroll } from 'react-scroll'
 
 export default function NavBar() {
+  const [isBlur, setIsBlur] = useState(false)
+  const router = useRouter();
+
+  function onScroll() {
+    setIsBlur(window.scrollY < 500);
+  }
+
+  function scrollBack() {
+    if (router.pathname == '/') {
+      scroll.scrollToTop()
+    } else {
+      router.push('/')
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
-    <div className={styles.bar}>
-      <strong className={styles.logo}>AZBITO</strong>
+    <div className={isBlur ? styles.bar : styles.blur}>
+      <strong className={styles.logo} onClick={scrollBack}>AZBITO</strong>
       <div className={styles.items}>
         <p className={styles.pages}>Home</p>
         <p className={styles.pages}>About</p>
--- src/pages/bio/index.tsx
+++ src/pages/bio/index.tsx
@@ -1,12 +1,106 @@
+import Image from 'next/image'
 import styles from '../../styles/bio.module.scss'
 
 export default function Bio() {
+  const firstRowTechnologies = [{
+    id: 1,
+    image: "images/javascript.svg",
+    alt: "Javascript",
+    width: 50,
+    height: 50
+  },
+  {
+    id: 2,
+    image: "images/typescript.svg",
+    alt: "Typescript",
+    width: 50,
+    height: 50
+  },
+  {
+    id: 3,
+    image: "images/react.svg",
+    alt: "React",
+    width: 50,
+    height: 50
+  },
+  {
+    id: 4,
+    image: "images/nextjs.svg",
+    alt: "Next.JS",
+    width: 50,
+    height: 50
+  }, {
+    id: 5,
+    image: "images/html.svg",
+    alt: "HTML",
+    width: 50,
+    height: 50
+  }, {
+    id: 6,
+    image: "images/sass.svg",
+    alt: "SASS",
+    width: 50,
+    height: 50
+  },
+  {
+    id: 7,
+    image: "images/css.svg",
+    alt: "CSS",
+    width: 50,
+    height: 50
+  },
+  {
+    id: 8,
+    image: "images/styledcomponents.svg",
+    alt: "Styled Components",
+    width: 50,
+    height: 50
+  },
+  {
+    id: 9,
+    image: "images/axios.svg",
+    alt: "Axios",
+    width: 45,
+    height: 50
+  },
+  {
+    id: 10,
+    image: "images/lua.svg",
+    alt: "Lua",
+    width: 50,
+    height: 50
+  }, {
+    id: 11,
+    image: "images/love2d.svg",
+    alt: "LÖVE",
+    width: 50,
+    height: 50
+  }]
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
+        <Image src="images/book.svg" alt="Book" width={80} height={80} />
         <h1>About me</h1>
         <div className={styles.description}>
-          <p>My name is Thiago and my nickname is Azbito. I'm 18, brazilian, front-end developer and artist. I'm able to talk in Portuguese, English and French. Studying coding since november, 2022, I decided to deepen in this branch.</p>
+          <p>My name is Thiago and my nickname is Azbito. I'm 18, brazilian, front-end developer and artist. I'm able to talk in Portuguese, English and French. I speak English since my 10 years old and French since my 16 years old. Studying coding since november, 2022, I decided to deepen in this branch.</p>
+        </div>
+        <h1>Technologies</h1>
+
+        <div className={styles.skills}>
+          {firstRowTechnologies.map((item) => (
+            <div key={item.id}>
+              <Image src={item?.image} alt={item?.alt} width={item?.width} height={item?.height} />
+            </div>
+          ))}
+        </div>
+        <div>
+          <h1>Tools</h1>
+          <div className={styles.tools}>
+            <Image src="images/vscode.svg" alt="VSCode" width={50} height={50} />
+            <Image src="images/windows.svg" alt="Windows" width={50} height={50} />
+            <Image src="images/linux.svg" alt="Linux" width={50} height={50} />
+          </div>
         </div>
       </div>
     </div>
