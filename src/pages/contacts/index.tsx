c4f947529ef0fc816a2eaed2591dda1a7d5b7a98
Thiago <azbitomsc@gmail.com>
2023-03-13 03:15:18 -0300

    Update

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,21 +1,24 @@
 import Button from "@/components/Button";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from './contacts.module.scss'
 
 export default function Contacts() {
+  const router = useRouter()
+
   return (
     <div className={styles.container}>
       <strong>Contacts</strong>
       <div className={styles.socialmedias}>
-        <div className={styles.instagram}>
+        <div className={styles.instagram} onClick={() => router.push('https://www.instagram.com/souoaz')}>
           <Image src="/images/instagram.png" alt="Instagram" height={25} width={25} />
           <h1>Instagram</h1>
         </div>
-        <div className={styles.github}>
+        <div className={styles.github} onClick={() => router.push('https://www.github.com/azbito')}>
           <Image src="/images/github.png" alt="GitHub" height={25} width={25} />
           <h1>GitHub</h1>
         </div>
-        <div className={styles.linkedin}>
+        <div className={styles.linkedin} onClick={() => router.push('https://www.linkedin.com/in/thiago-roque-b39078236/')}>
           <Image src="/images/linkedin.png" alt="Linkedin" height={25} width={25} />
           <h1>LinkedIn</h1>
         </div>
