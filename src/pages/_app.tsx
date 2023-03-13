198ad7159e62d02d124eec960df6aac063062a18
Thiago <101950809+Azbito@users.noreply.github.com>
2023-03-13 14:39:46 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,7 @@ import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-
-    {/* <NavBar /> */}
+    <NavBar />
     <Component {...pageProps} />
   </>
 }
