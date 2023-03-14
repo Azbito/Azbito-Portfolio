8967b465aff7ce4f7c0aa221d7c0dd29f2843a14
Thiago <101950809+Azbito@users.noreply.github.com>
2023-03-14 19:17:52 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import NavBar from '@/components/NavBar'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
