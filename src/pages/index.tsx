82f7384029f10bbb6f7a0ff62880c48cc559befb
Thiago <azbitomsc@gmail.com>
2023-03-18 03:01:19 -0300

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.container}>
-        <Image className={styles.picture} src="/images/photo.jpg" alt="Me" width={500} height={500} />
+        <Image className={styles.picture} src="/images/photo.jpg" alt="Me" width={200} height={200} />
         <div className={styles.summary}>
           <h1 className={styles.title}>Summary</h1>
           <p className={styles.description}>Ahoy! My name is Thiago and also my nickname is Azbito. I'm a Brazilian front-end developer and pixelartist. I'm always looking forward to improvement.</p>
