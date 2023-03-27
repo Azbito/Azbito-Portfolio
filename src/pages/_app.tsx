79c3b059e27a5e9c5bf4d862747fbe7440b65547
Azbito <azbitomsc@gmail.com>
2023-03-27 14:56:26 -0300

    CHORE: Loader component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,35 @@
+import Loader from '@/components/Loader'
 import NavBar from '@/components/NavBar'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Router } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <NavBar />
-    <Component {...pageProps} />
-  </>
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const start = () => {
+      setIsLoading(true)
+    };
+    const end = () => {
+      setIsLoading(false)
+    };
+    Router.events.on("routeChangeStart", start)
+    Router.events.on("routeChangeComplete", end)
+    Router.events.on("routeChangeError", end)
+    return () => {
+      Router.events.off("routeChangeStart", start)
+      Router.events.off("routeChangeComplete", end)
+      Router.events.off("routeChangeError", end)
+    }
+  }, [])
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
